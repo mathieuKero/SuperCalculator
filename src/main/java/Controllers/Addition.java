5becc9c1a96d9068561bc2003a4d665b349bd88f
mathieuKero <m.keromnes@iia-laval.fr>
2020-10-20 13:37:08 +0200

    Ajout tests unitaire Addition

---
+++ src/main/java/Controllers/Addition.java
@@ -0,0 +1,8 @@
+package Controllers;
+
+public class Addition {
+	
+	public int Addition(int a, int b) {
+		return 1;
+	}
+}
---
+++ src/test/java/Tests/AdditionTest.java
@@ -0,0 +1,33 @@
+package Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import Controllers.Addition;
+
+public class AdditionTest {
+
+	private Addition add;
+	
+	@Before
+	public void setUp() throws Exception {
+		add = new Addition();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void AdditionTwoNumbers() {
+		int result = this.add.Addition(1, 2);
+		Assert.assertEquals(3, result);
+	}
+
+}
