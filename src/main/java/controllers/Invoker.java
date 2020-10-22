b7b5eb57d53330648f7859f52d6d332a3d578b41
mathieuKero <m.keromnes@iia-laval.fr>
2020-10-22 14:02:40 +0200

    ajout doc

--- src/main/java/controllers/Invoker.java
+++ src/main/java/controllers/Invoker.java
@@ -2,19 +2,38 @@ package controllers;
 
 import java.util.ArrayList;
 
+/**
+ * Invoker that control all of the controls (add / sub / div / mul)
+ * When a control is created, the invoker class will dispatch the order to the corresponding command
+ * @author Mathieu K
+ */
 public class Invoker {
  
-	private ArrayList<Command> listCommands;
+	/**
+	 * List of commands that are stored in order to get the history
+	 */
+	final private ArrayList<Command> listCommands;
 	
+	/**
+	 * Create a new instance of the Invoker class
+	 */
 	public Invoker() {
-		listCommands = new ArrayList<Command>();
+		listCommands = new ArrayList<>();
 	}
 	
-	public void doCommand(Command command) {
+	/**
+	 * Call the command method for all the inherited classes.
+	 * @param command command that will be managed
+	 */
+	public void doCommand(final Command command) {
 		command.calculate();
 		listCommands.add(command);
 	}
 
+	/**
+	 * Return an instance of historic
+	 * @return list of Commands
+	 */
 	public ArrayList<Command> getHitsoric(){
 		return this.listCommands;
 	}
